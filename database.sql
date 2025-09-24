00b5cbf3059cf56f1113f122e99a580a3cfcd5f6
NoneTheWeisser <214901165+NoneTheWeisser@users.noreply.github.com>
2025-09-24 15:14:46 -0500

    get route entered

--- database.sql
+++ database.sql
@@ -1,5 +1,6 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
 CREATE TABLE shopping (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(80) NOT NULL,
---
+++ server/routes/shopping.router.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const pool = require('../modules/pool')
+const router = express.Router()
+
+
+// GET
+router.get('/', (req, res) =>{
+    const sqlText = `SELECT * FROM "shopping" ORDER by "id"'`
+
+    pool.query(sqlText)
+    .then((result) => {
+        console.log(`Shopping list from database`, result.rows);
+        res.send(result.rows );
+    })
+    .catch((error) => {
+        console.log(`Error making database query ${sqlText}`, error);
+        res.sendStatus(500);
+    })
+})
+
+// POST
+
+
+// DELETE
