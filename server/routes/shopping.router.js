bf0a9a4ed9087719f834a9393ecb5e511dff2185
ABDIWAHAB NUNOW <abdiwahabnunow@ABDIWAHABs-MacBook-Air.local>
2025-09-24 15:32:31 -0500

    post done for review

--- server/routes/shopping.router.js
+++ server/routes/shopping.router.js
@@ -19,6 +19,17 @@ router.get('/', (req, res) =>{
 })
 
 // POST
+router.post('/', (req, res) => {
+    console.log('POST req.body', req.body);
+    let queryText = 'INSERT INTO "shopping" ("Name", "Quantity") VALUES ($1, $2);'
+    pool.query(queryText, [req.body.name, req.body.role])
+    .then((result) => {
+        res.sendStatus(200);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
+});
 
 
 // DELETE
