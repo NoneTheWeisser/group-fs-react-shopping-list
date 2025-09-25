77aff2b704febb63f7a38383d6393d53b15b8ee5
NoneTheWeisser <214901165+NoneTheWeisser@users.noreply.github.com>
2025-09-25 09:11:54 -0500

    added a few more styling changes and cleaned up the readme

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ListItem from "./ListItem";
+import './ShoppingList.css';
 
 const ShoppingList = ({ shoppingList }) => {
   return (
     <div className="container py-4">
       <h2 className="text-center mb-4 text-primary fw-bold">Shopping List</h2>
-
       <table className="table table-striped table-bordered">
         <thead className="table-dark">
           <tr>
